f4375ff9c0a63f5d7ccd7ab700b9f28b04d93c32
Tyler Gener <tylergen33@gmail.com>
2022-09-07 15:12:10 -0700

    create playRound function

--- script.js
+++ script.js
@@ -1,13 +1,58 @@
+let playerScore = 0;
+let computerScore = 0;
+
+// Randomly generating the computer choice
+
 let getComputerChoice = () => {
   const randomNum = Math.floor(Math.random() * 3) + 1;
 
-  if (randomNum === 1) {
+  if (randomNum == 1) {
     return "rock";
-  } else if (randomNum === 2) {
+  } else if (randomNum == 2) {
     return "paper";
   } else {
     return "scissors";
   }
 };
 
-console.log(getComputerChoice());
+// Playing a single round of the game
+
+let playRound = (playerSelection, computerSelection) => {
+  if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "scissors" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerSelection == "rock")
+  ) {
+    playerScore += 1;
+    return (
+      "You won " +
+      playerSelection +
+      " beats " +
+      computerSelection +
+      "!" +
+      "\n\nPlayer score: " +
+      playerScore +
+      "\nComputer Score :" +
+      computerScore
+    );
+  } else if (playerSelection == computerSelection) {
+    return "It's a draw! You both picked " + playerSelection + "!";
+  } else {
+    computerScore += 1;
+    return (
+      "You Lose! " +
+      computerSelection +
+      " beats " +
+      playerSelection +
+      "!" +
+      "\n\nPlayer score: " +
+      playerScore +
+      "\nComputer Score :" +
+      computerScore
+    );
+  }
+};
+
+const playerSelection = prompt("Rock Paper or Scissors").toLowerCase();
+computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
